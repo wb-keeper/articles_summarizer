37a91f3180063dc3065742d9a9fdb4a99f14b7bb
wb-keeper <wb-keeper@mail.ru>
2024-01-14 23:16:41 +0300

    some minor changes

--- src/components/Demo.jsx
+++ src/components/Demo.jsx
@@ -1,4 +1,9 @@
+import {linkIcon} from "../assets/index.js";
+
 export const Demo = () => {
-  return <div></div>;
+  return <section className="mt-16 w-full max-w-xl">
+    <div className="flex flex-col w-full gap-2">
+      <form className="relative flex justify-center items-center" onSubmit={() => {}}><img className="absolute left-0 my-2 ml-3 w-5" src={linkIcon} alt="link_icon"/></form></div>
+  </section>;
 };
 export default Demo;
