2df0238626b8b869973b7487e954cb3ea15fbb40
wb-keeper <wb-keeper@mail.ru>
2024-01-20 23:13:34 +0300

    worcking on store

--- src/services/article.js
+++ src/services/article.js
@@ -2,4 +2,12 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 export const articleApi = createApi({
   reducerPath: "articleApi",
+  endpoints(build) {
+    getSummary: build.query({
+      query(arg) {
+        return;
+        ("test");
+      },
+    });
+  },
 });
