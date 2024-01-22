9ec20aea75fb377b4efd0774683525809d89c0aa
wb-keeper <wb-keeper@mail.ru>
2024-01-22 22:31:10 +0300

    working on rapidapi

--- src/services/article.js
+++ src/services/article.js
@@ -1,12 +1,27 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
+const options = {
+  method: "GET",
+  url: "https://article-extractor-and-summarizer.p.rapidapi.com/summarize",
+  params: {
+    url: "https://time.com/6266679/musk-ai-open-letter/",
+    length: "3",
+  },
+  headers: {
+    "X-RapidAPI-Key": "73270bb7eamsh9e55a5460de75fbp1769d8jsna77335678dff",
+    "X-RapidAPI-Host": "article-extractor-and-summarizer.p.rapidapi.com",
+  },
+};
+
 export const articleApi = createApi({
   reducerPath: "articleApi",
+  baseQuery: fetchBaseQuery({
+    baseUrl: "",
+  }),
   endpoints(build) {
     getSummary: build.query({
-      query(arg) {
-        return;
-        ("test");
+      query(params) {
+        return "test";
       },
     });
   },
