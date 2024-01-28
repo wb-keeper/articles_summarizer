22c068e63abd204688bb5b5efa4e7dde1d591b76
wb-keeper <wb-keeper@mail.ru>
2024-01-28 22:54:32 +0300

    some minor changes

--- src/components/Demo.jsx
+++ src/components/Demo.jsx
@@ -4,10 +4,17 @@ import { useLazyGetSummaryQuery } from "../services/article.js";
 
 export const Demo = () => {
   const [article, setArticle] = useState({ url: "", summary: "" });
-  const handleSubmit = async (e) => {
-    alert("Submitted");
-  };
   const [getSummary, { error, isFetching }] = useLazyGetSummaryQuery();
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const { data } = await getSummary({ articleUrl: article.url });
+    if (!data.summary) {
+      const newArticle = { ...article, summary: data.summary };
+      setArticle(newArticle);
+      console.log(newArticle);
+    }
+  };
+
   return (
     <section className="mt-16 w-full max-w-xl">
       <div className="flex flex-col w-full gap-2">
