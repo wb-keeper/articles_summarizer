36ce72084ed290e5d6f66a2e462d55f9b4c7cc22
wb-keeper <wb-keeper@mail.ru>
2024-02-01 22:39:40 +0300

    Working on output of Articles

--- src/components/Demo.jsx
+++ src/components/Demo.jsx
@@ -53,7 +53,24 @@ export const Demo = () => {
             ←
           </button>
         </form>
-        <div className="flex flex-col gap-1 max-h-60 overflow-y-auto"></div>
+        <div className="flex flex-col gap-1 max-h-60 overflow-y-auto">
+          {allArticles.map((item, index) => (
+            <div
+              key={index}
+              onClick={() => setArticle(item)}
+              className="link_card"
+            >
+              <div className="copy_btn">
+                <img
+                  src={"copy"}
+                  alt="copy icon"
+                  className="w-[40%] h-[40%] object-contain"
+                />
+              </div>
+              <p className="flex-1 font-satoshi text-blue-50">{item.url}</p>
+            </div>
+          ))}
+        </div>
       </div>
     </section>
   );
