70210ab747d26608863d2f785f02204c3d552feb
wb-keeper <wb-keeper@mail.ru>
2024-02-03 22:56:37 +0300

    displaying errors

--- src/components/Demo.jsx
+++ src/components/Demo.jsx
@@ -75,8 +75,27 @@ export const Demo = () => {
         </div>
       </div>
       <div className="my-10 max-w-full flex justify-center items-center">
-        {isFetching ? (<img src={loader} alt="loader" className="w-20 h-20 object-contain" />) : error ? (<p className="font-inter font font-bold text-black text-center">Something went wrong <br/>
-        <span>{error?.data?.error}</span></p> )}
+        {isFetching ? (
+          <img src={loader} alt="loader" className="w-20 h-20 object-contain" />
+        ) : error ? (
+          <p className="font-inter font font-bold text-black text-center">
+            Something went wrong <br />
+            <span className="font-satoshi font-normal text-gray-700">
+              {error?.data?.error}
+            </span>
+          </p>
+        ) : (
+          article.summary && (
+            <div className="flex flex-col gap-3">
+              <h2 className="font-bold font-satosh text-gray-600 text-xl">
+                Article <span className="blue_gradient"> Summary</span>
+              </h2>
+              <div className="summary_box">
+                <p>{article.summary}</p>
+              </div>
+            </div>
+          )
+        )}
       </div>
     </section>
   );
